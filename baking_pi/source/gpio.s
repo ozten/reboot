87a32c6430e2d0bffd950c709541aeae61ba9642
Austin King <shout@ozten.com>
2014-08-10 10:07:36 -0700

    Chapter 3 of baking pi

---
+++ baking_pi/source/gpio.s
@@ -0,0 +1,63 @@
+/* return the address of the GPIO pin */
+.globl GetGpioAddress
+GetGpioAddress:
+	ldr r0,=0x20200000
+	mov pc,lr
+
+/* Set the function of the GPIO register address in r0
+ * to the low 3 bits of r1 */
+.globl SetGpioFunction
+SetGpioFunction:
+	/* Validate that r0 is in the range 0 - 53
+	   Validate that r1 is in the range 0 - 7 */
+	cmp r0,#53
+	cmpls r1,#7
+	movhi pc,lr
+	push {lr}
+	mov r2,r0
+	bl GetGpioAddress
+
+	functionLoop$:
+		cmp r2,#9
+		subhi r2,#10
+		addhi r0,#4
+		bhi functionLoop$
+
+	add r2, r2,lsl #1
+	lsl r1,r2
+	str r1,[r0]
+	pop {pc}
+
+/* Set a GPIO pin to a value
+ * r0 GPIO register address
+ * r1 value - 0 or 1
+ */
+.globl SetGpio
+SetGpio:
+	pinNum .req r0 /* Register Alias, pinNum now means r0 */
+	pinVal .req r1
+	cmp pinNum,#53
+	movhi pc,lr
+	push {lr}
+	mov r2,pinNum
+	.unreq pinNum
+	pinNum .req r2
+	bl GetGpioAddress
+	gpioAddr .req r0
+	pinBank .req r3
+	lsr pinBank,pinNum,#5
+	lsl pinBank,#2
+	add gpioAddr,pinBank
+	.unreq pinBank
+	and pinNum,#31
+	setBit .req r3
+	mov setBit,#1
+	lsl setBit,pinNum
+	.unreq pinNum
+	teq pinVal,#0
+	.unreq pinVal
+	streq setBit,[gpioAddr,#40]
+	strne setBit,[gpioAddr,#28]
+	.unreq setBit
+	.unreq gpioAddr
+	pop {pc}
--- baking_pi/source/main.s
+++ baking_pi/source/main.s
@@ -1,32 +1,54 @@
 .section .init
 .globl _start
 _start:
-ldr r0,=0x20200000
+b main
 
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
+.section .text
+
+main:
+mov sp,#0x800 /* See ../kernel.ld 0x800 is the start of .init */
+
+
+/* Set up GPIO port 16 in binary mode (001) */
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
 
 loop$:
 
-mov r1,#1
-lsl r1,#16
-str r1,[r0,#40]
+	/* Turn off LED */
+	pinNum .req r0
+	pinVal .req r1
+	mov pinNum,#16
+	mov pinVal,#0
+	bl SetGpio
+	.unreq pinNum
+	.unreq pinVal
 
-mov r2,#0x3F0000
-wait1$:
-sub r2,#1
-cmp r2,#0
-bne wait1$
+	/* Sleep 2 seconds */
+	mov r2,#0x3F0000
+	wait1$:
+		sub r2,#1
+		cmp r2,#0
+		bne wait1$
 
-mov r1,#1
-lsl r1,#16
-str r1,[r0,#28]
+	/* Turn on the LED */
+	pinNum .req r0
+	pinVal .req r1
+	mov pinNum,#16
+	mov pinVal,#1
+	bl SetGpio
+	.unreq pinNum
+	.unreq pinVal
 
-mov r2,#0x3F0000
-wait2$:
-sub r2,#1
-cmp r2,#0
-bne wait2$
-
+	/* Sleep 2 seconds */
+	mov r2,#0x3F0000
+	wait2$:
+		sub r2,#1
+		cmp r2,#0
+		bne wait2$
 b loop$
