6bf71408c9cf74180bc87bc56e90dd9d96a25f62
Austin King <shout@ozten.com>
2014-08-16 13:31:42 -0700

    Chap 5 - more blinking

--- baking_pi/source/main.s
+++ baking_pi/source/main.s
@@ -5,6 +5,12 @@ _start:
 
 .section .text
 
+	ptrn .req r4
+	ldr ptrn,=pattern
+	ldr ptrn,[ptrn]
+	seq .req r5
+	mov seq,#0
+
 main:
     mov sp,#0x800 /* See ../kernel.ld 0x800 is the start of .init */
 
@@ -24,31 +30,35 @@ main:
 
 loop$:
 
-turn_off_led:	
+
+
+turn_on_or_off_led:	
 	/* Turn off LED */
 	pinNum .req r0
 	pinVal .req r1
 	mov pinNum,#16
-	mov pinVal,#0
-	bl SetGpio
-	.unreq pinNum
-	.unreq pinVal
 
-	mov r0, #3 	@ldr r0,=0x200B20          @ 700 which is 1 millisecond 0x3F000 is about 6 seconds (5898.24 = 4128768 / 700)
-	bl Sleep
-	
-turn_on_led:	
-
-	/* Turn on the LED */
-	pinNum .req r0
-	pinVal .req r1
-	mov pinNum,#16
+read_pattern:	
 	mov pinVal,#1
+	lsl pinVal,seq
+	and pinVal,ptrn	
+
 	bl SetGpio
 	.unreq pinNum
 	.unreq pinVal
 
-	mov r0, #4      	@ldr r0,=0x3F0000          @ 700 which is 1 millisecond 0x3F000 is about 6 seconds (5898.24 = 4128768 / 700)
-	bl Sleep	
+	mov r0, #1 	@ldr r0,=0x200B20          @ 700 which is 1 millisecond 0x3F000 is about 6 seconds (5898.24 = 4128768 / 700)
+	bl Sleep
 
+update_pattern_seq:
+	add seq, seq, #1
+	cmp seq,#31
+	movgt seq,#0
+	
 	b loop$
+
+	.section .data
+	.align 2
+pattern:
+	.int 0b11111111101010100010001000101010
+	
